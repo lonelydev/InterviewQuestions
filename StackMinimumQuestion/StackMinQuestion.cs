cec502c461c806ee233723b7160115adf412109e
Eakan Gopalakrishnan <me_theonlyone@hotmail.com>
2017-12-25 15:36:22 +0000

    feat(getMinFromStackO1): Another GFG question
    Get min element from stack in O(1) time.

---
+++ StackMinimumQuestion/StackMinQuestion.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace StackMinimumQuestion
+{
+    /*
+     * Please note that it's Function problem i.e.
+     * you need to write your solution in the form of Function(s) only.
+     * Driver Code to call/invoke your function would be added by GfG's Online Judge.
+     */
+
+    public class StackMinQuestion
+    {
+        Stack<int> s = new Stack<int>();
+        Stack<int> minElements = new Stack<int>();
+
+        /// <summary>
+        /// Returns min element from stack
+        /// </summary>
+        /// <returns></returns>
+        public int GetMin()
+        {
+            if (!s.Any())
+            {
+                return -1;
+            }
+            return minElements.Peek();
+        }
+
+        /// <summary>
+        /// Returns popped element from stack
+        /// </summary>
+        /// <returns></returns>
+        public int Pop()
+        {
+            if (!s.Any())
+            {
+                return -1;
+            }
+            minElements.Pop();
+            return s.Pop();
+        }
+
+        /// <summary>
+        /// push element x into the stack
+        /// if x is greater than the top minelement then 
+        /// push the top min element into the minelement stack again
+        /// then insert x into stack
+        /// If x is less or equal to top minelement then just push x into
+        /// the minelements stack and so into the actual stack
+        /// In short whether small or not you have to make sure you insert something into the 
+        /// minElement stack so that your pop method is simple. 
+        /// All the complexity of tracking minElement is in push. 
+        /// </summary>
+        /// <param name="x"></param>
+        public void Push(int x)
+        {
+            if (minElements.Any())
+            {
+                if (minElements.Peek() < x)
+                {
+                    minElements.Push(minElements.Peek());
+                    s.Push(x);
+                }
+                else
+                {
+                    minElements.Push(x);
+                    s.Push(x);
+                }
+            }
+            else
+            {
+                minElements.Push(x);
+                s.Push(x);
+            }
+        }
+    }
+}
---
+++ StackMinimumQuestionTester/StackMinTester.cs
@@ -0,0 +1,72 @@
+﻿using NUnit.Framework;
+using StackMinimumQuestion;
+
+namespace StackMinimumQuestionTester
+{
+    [TestFixture]
+    public class StackMinTester
+    {
+        [Test]
+        public void StackMinTest1()
+        {
+            var gfg = new StackMinQuestion();
+            gfg.Push(10);
+            gfg.Push(5);
+            gfg.Push(7);
+            gfg.Push(9);
+            gfg.Push(4);
+            Assert.AreEqual(4, gfg.GetMin());
+
+            gfg.Pop();
+            Assert.AreEqual(5, gfg.GetMin());
+        }
+
+        [Test]
+        public void StackMinTest2()
+        {
+            var gfg = new StackMinQuestion();
+            gfg.Push(10);
+            gfg.Push(9);
+            gfg.Push(8);
+            gfg.Push(7);
+            gfg.Push(6);
+            Assert.AreEqual(6, gfg.GetMin());
+
+            gfg.Pop();
+            Assert.AreEqual(7, gfg.GetMin());
+
+
+            gfg.Pop();
+            Assert.AreEqual(8, gfg.GetMin());
+
+
+            gfg.Pop();
+            Assert.AreEqual(9, gfg.GetMin());
+        }
+
+        [Test]
+        public void StackMinTest3()
+        {
+            var gfg = new StackMinQuestion();
+            gfg.Push(10);
+            Assert.AreEqual(10, gfg.GetMin());            
+        }
+
+        [Test]
+        public void StackMinTest4()
+        {
+            var gfg = new StackMinQuestion();            
+            Assert.AreEqual(-1, gfg.GetMin());
+        }
+
+        [Test]
+        public void StackMinTest5()
+        {
+            var gfg = new StackMinQuestion();
+            gfg.Push(10);
+            Assert.AreEqual(10, gfg.GetMin());
+            gfg.Pop();
+            Assert.AreEqual(-1, gfg.GetMin());
+        }
+    }
+}
