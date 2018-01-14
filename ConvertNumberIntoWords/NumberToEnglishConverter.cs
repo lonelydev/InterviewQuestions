4523eb4013c18d75336813e12f1b6729d4d2b74c
Eakan Gopalakrishnan <me_theonlyone@hotmail.com>
2018-01-14 19:30:39 +0000

    feat(numberToWords): Interesting problem of converting a number to English words
    Just adding the question and readme and skeleton. will implement the solution and tests in another commit.

---
+++ ConvertNumberIntoWords/NumberToEnglishConverter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConvertNumberIntoWords
+{
+    public class NumberToEnglishConverter
+    {
+        public static string ToEnglish(int number)
+        {
+            return "";
+        }
+    }
+}
---
+++ ConvertNumberIntoWords/README.md
@@ -0,0 +1,5 @@
+﻿# Convert number into words
+
+Write a program that converts a given integer number into words. 
+
+For example, if `1234` is given as input, output should be `one thousand two hundred thirty four`.
