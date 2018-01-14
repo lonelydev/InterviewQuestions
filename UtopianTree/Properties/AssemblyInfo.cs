2b17b9e0b5c1d6f5c98849f638b032dea914dbcb
Eakan Gopalakrishnan <me_theonlyone@hotmail.com>
2018-01-14 18:18:58 +0000

    feat(utopianTree): adding utopian tree question
    Adding the utopian tree question while I remember.
    Will implement the answer later. I have more questions to add.

---
+++ UtopianTree/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("UtopianTree")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UtopianTree")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("be92673e-8c06-4d11-9970-5f0d682a66c8")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UtopianTree/README.md
@@ -0,0 +1,21 @@
+﻿# Utopian Tree
+
+Once upon a time in Utopia, there was this wonderful tree that people were fond of. 
+It had a very interesting growth pattern. Every year, `it had two growth cycles`.
+One during monsoon and the other during summer. 
+
+  * During monsoon the tree grew `twice its height`
+  * During summer it grew just `1 additional meter`
+
+If a utopian citizen were to plant a sapling of this tree of `1 meter` height in the onset of monsoon, could you tell what the height of the tree would be after `n` growth cycles would be?
+
+## Key concepts
+
+  * Count from the onset of monsoon. 
+  * Initial height is `1 meter`
+  * During monsoon, it grows twice its height. So by the end of first growth cycle it will be `2 meters` tall.
+  * During summer, it grows an additional meter. So after 2 growth cycles it will be `3 meters` tall.
+  * So growth can be categorized into two cycles:
+    * Odd cycle - `2 x height`
+    * Even cycle - `height + 1`
+
