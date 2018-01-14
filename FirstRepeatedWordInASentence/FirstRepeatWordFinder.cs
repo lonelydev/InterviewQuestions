e0f47b3422cd7fcdcc5a07b468f4cad2fb597872
Eakan Gopalakrishnan <me_theonlyone@hotmail.com>
2018-01-14 18:26:15 +0000

    feat(firstRepeatWordFinder): Find first repeated word in a sentence
    This is a fairly simple question that gets asked more frequently than you think.

---
+++ FirstRepeatedWordInASentence/FirstRepeatWordFinder.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstRepeatedWordInASentence
+{
+    public class FirstRepeatWordFinder
+    {
+        static string FirstRepeatedWord(string sentence)
+        {
+            var wordRepCounter = new Dictionary<string, int>();
+            var delimiters = new char[] { ' ', ',', ':', '-', '.', '\t' };
+            string firstRepeatWord = null;
+            foreach (var word in sentence.Split(delimiters))
+            {
+                if (string.IsNullOrEmpty(word) || string.IsNullOrWhiteSpace(word))
+                    continue;
+                if (wordRepCounter.ContainsKey(word))
+                {
+                    firstRepeatWord = word;
+                    break;
+                }
+                else
+                {
+                    wordRepCounter.Add(word, 1);
+                }
+            }
+            return firstRepeatWord;
+        }
+    }
+}
---
+++ FirstRepeatedWordInASentence/README.md
@@ -0,0 +1,9 @@
+﻿# First Repeated Word
+
+A sentence is a group of words. A word is a sequence of characters or letters `a-zA-Z`.
+Words are delimited by spaces or non-leter characters like `',', ':', '-', '.', '\t'`.
+
+A repeated word is when a word occurs more than once in a sentence provided the comparison is case sensitive. 
+For example, `had != Had`.
+
+Given a sentence as a string, find the first repeated word in the string. 
