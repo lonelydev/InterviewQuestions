e9c65fa33ddb08af3759508abb35ebee6ff32e07
Eakan Gopalakrishnan <me_theonlyone@hotmail.com>
2018-01-16 12:41:30 +0000

    feat(linkedList): Removal of duplicates from a linked list
    Solution and tests added.
    Readme updated for now.
    However there are other solutions to be added to the same project.

---
+++ CTCI-LinkedList/LinkedList_1TestInput.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace CTCI_LinkedList
+{
+    public class LinkedList_1TestInput
+    {
+        public static IEnumerable UnsortedLinkedListTestData
+        {
+            get
+            {
+                yield return 
+                    new TestCaseData(
+                        new List<int> { 90, 10, 8, 15, 8, 20, 85, 77, 89, 91, 90 }, 
+                        "90->10->8->15->20->85->77->89->91->NULL");
+
+                yield return
+                    new TestCaseData(
+                        new List<int> { 13, 10, 8, 15, 15, 10, 85, 77, 91, 77, 13 },
+                        "13->10->8->15->85->77->91->NULL");
+
+                yield return
+                    new TestCaseData(
+                        new List<int> { 13, 10, 8, 10, 15, 85, 85, 77, 91, 13, 91 },
+                        "13->10->8->15->85->77->91->NULL");
+
+                yield return
+                    new TestCaseData(
+                        new List<int> { 13, 13, 13, 13, 10, 13, 13, 10, 13, 13 },
+                        "13->10->NULL");
+
+                yield return
+                    new TestCaseData(
+                        new List<int> { 13, 13, 13, 13, 13 },
+                        "13->NULL");
+            }            
+        }
+    }
+}
---
+++ CTCI-LinkedList/README.md
@@ -0,0 +1,27 @@
+﻿# Cracking the Coding Interview Series - Linked List Problems
+
+I have decided to solve the problems in the CTCI book. Every chapter will be a different project in the solution. 
+This is the one on LinkedLists and I am hoping to tackle all the 7 linked list questions that can possibly be solved in C#.
+
+## Duplicate Removal
+
+Write a program to remove duplicate nodes from an unsorted linked list. 
+
+Followup
+
+How would you achieve this if a temporary buffer was not allowed?
+
+Solutions in `LinkedList_1.cs`
+
+## Value of kth to the last element
+
+Implement an algorithm to find the k'th to the last element of a singly linked list. 
+
+Solutions in `LinkedList_2.cs`
+
+## Delete a node in the middle
+
+Implement an algorithm to delete a node in the middle of a singly linked list, give only acess to that node.
+
+Solutions in `LinkedList_3.cs`
+
