e1f5fc8a4d914db56a60109e8221156f3b3a7a0e
Eakan Gopalakrishnan <me_theonlyone@hotmail.com>
2018-01-17 15:36:41 +0000

    feat(arrayPairSum): Find number of pairs in array that add upto sum.
    solution and tests.

---
+++ ArrayPairSumK/CountPairsWithSumTester.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using NUnit.Framework;
+
+namespace ArrayPairSumK
+{
+    [TestFixture]
+    public class CountPairsWithSumTester
+    {
+        [Test, TestCaseSource(typeof(CountPairsWithSumTesterInput), "IntArrayAndSumK")]
+        public int TestCountOfPairsWithSums(int[] array, int sum)
+        {
+            return CountPairsWithSumSolver.CountNumberPairsWithSumK(array, sum);
+        }
+    }
+
+    public class CountPairsWithSumTesterInput
+    {
+        public static IEnumerable IntArrayAndSumK
+        {
+            get
+            {
+                yield return new TestCaseData(new int[] { 10, 15, 25, 15, 30, 5 }, 35).Returns(2);
+                yield return new TestCaseData(new int[] { 1, 5, 7, -1, 5 }, 6).Returns(3);
+                yield return new TestCaseData(new int[] { 1, 1, 1, 1 }, 2).Returns(6);
+                yield return new TestCaseData(new int[] {10, 12, 10, 15, -1, 7, 6,
+                   5, 4, 2, 1, 1, 1}, 11).Returns(9);
+
+            }
+        }
+    }
+}
---
+++ ArrayPairSumK/README.md
@@ -0,0 +1,28 @@
+﻿# Count pairs with a given sum in an array
+
+Given an array of integers, and a number `sum`, find the number of pairs of integers in the array whose sum is equal to `sum`.
+
+    Examples:
+    Input  :  arr[] = {1, 5, 7, -1}, 
+              sum = 6
+    Output :  2
+    Pairs with sum 6 are (1, 5) and (7, -1)
+
+    Input  :  arr[] = {1, 5, 7, -1, 5}, 
+              sum = 6
+    Output :  3
+    Pairs with sum 6 are (1, 5), (7, -1) &amp;
+                         (1, 5)         
+
+    Input  :  arr[] = {1, 1, 1, 1}, 
+              sum = 2
+    Output :  6
+    There are 3! pairs with sum 2.
+
+    Input  :  arr[] = {10, 12, 10, 15, -1, 7, 6, 
+                       5, 4, 2, 1, 1, 1}, 
+              sum = 11
+    Output :  9
+
+Expected time complexity O(n)
+
