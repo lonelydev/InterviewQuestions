b845d6f7a367e4151b2157216461a12d68812e84
Eakan Gopalakrishnan <me_theonlyone@hotmail.com>
2018-01-17 20:00:42 +0000

    feat(longestUniformSubstring): A simple but tricky problem
    Always try to solve the problem without using a complex datastructure.
    In this case, the solution can be achieved simply by using String.Substring function.
    
    Lesson learned.

---
+++ LongestContinuousStringOfRepeatingCharsQuestion/LongestContinuousStringOfRepeatingCharsTest.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using LongestContinuousStringOfRepeatingChars.Solution;
+
+namespace LongestContinuousStringOfRepeatingChars.Test
+{
+    [TestFixture]
+    public class LongestUniformStringTest
+    {
+        [Test, TestCaseSource(typeof(LongestUniformStringTestInput), "TestCases")]
+        public List<string> SubstringOfStringRepTest1(string input)
+        {
+            return LongestUniformString.GetRepititiveCharSubstringsAndLengths(input);
+        }
+
+        [Test, TestCaseSource(typeof(LongestUniformStringTestInput), "LongestUniformSubstringTuple")]
+        public Tuple<int, string> LongestUniformSubstringTester(string input)
+        {
+            return LongestUniformString.GetLongestUniformubstringsAndStartIndex(input);
+        }
+    }
+
+    public class LongestUniformStringTestInput
+    {
+        public static IEnumerable TestCases
+        {
+            get
+            {
+                yield return new TestCaseData("abbbbbaaafccccccccdd").Returns(new List<string> { "a", "bbbbb", "aaa", "f", "cccccccc", "dd" });
+                yield return new TestCaseData("geeksforgeeks").Returns(new List<string> { "g", "ee", "k", "s", "f", "o", "r", "g", "ee", "k", "s" });
+            }
+        }
+
+        public static IEnumerable LongestUniformSubstringTuple
+        {
+            get
+            {
+                yield return new TestCaseData("abbbbbaaafccccccccdd").Returns(new Tuple<int, string>(10, "cccccccc"));
+                yield return new TestCaseData("geeksforgeeks").Returns(new Tuple<int, string>(1, "ee"));
+            }
+        }
+    }
+}
