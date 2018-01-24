44da132fcfe3ef3099a5eca13eb606179235512e
Eakan Gopalakrishnan <me_theonlyone@hotmail.com>
2018-01-24 20:31:15 +0000

    chore(fibonacci/consolidate): tests and questions
    All done.

--- FibonacciQuestion/Tests/FibonacciTester.cs
+++ FibonacciQuestion/Tests/FibonacciTester.cs
@@ -1,8 +1,8 @@
 ﻿using System.Collections.Generic;
 using NUnit.Framework;
-using FibonacciQuestion;
+using FibonacciQuestion.Problem;
 
-namespace FibonacciQuestionTester
+namespace FibonacciQuestionTester.Tests
 {
     [TestFixture]
     public class FibonacciTester
