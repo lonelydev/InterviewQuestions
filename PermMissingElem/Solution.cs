5d8c8ccd6866028f2ae806ba0212557f87ce4de0
Eakan Gopalakrishnan <me_theonlyone@hotmail.com>
2021-07-11 22:18:04 +0100

    chore(unit-test): Install MSTest SDK to run NUnit tests in VS2019

---
+++ PermMissingElem/Solution.cs
@@ -0,0 +1,22 @@
+﻿namespace PermMissingElem
+{
+    public class Solution
+    {
+        public int Solve(int[] A)
+        {
+            // write your code in C# 6.0 with .NET 4.5 (Mono)
+            var actualSum = 0;
+            var max = 0;
+            for (int i = 0; i < A.Length; i++)
+            {
+                if (A[i] > max)
+                {
+                    max = A[i];
+                }
+                actualSum += A[i];
+            }
+            var expectedSum = (max * (max + 1)) / 2;
+            return expectedSum - actualSum;
+        }
+    }
+}
